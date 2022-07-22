c33ff4607688b7506428e0766a8795471e00d917
rainware <kevin90116@gmail.com>
2022-07-22 22:55:15 +0800

    U: remove temporary strings

--- examples/settings.py
+++ examples/settings.py
@@ -21,7 +21,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'test',
         'USER': 'root',
-        'PASSWORD': 'chenjian',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {'charset': 'utf8mb4'}
